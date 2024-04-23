2145d32e60fb03d96e736b1b67d86074ce0255e2
sudhanshuJha01 <sudhanshujha883@gmail.com>
2024-04-23 22:37:02 +0530

    js 01 project

---
+++ 01_js_Drum/index.js
@@ -0,0 +1,19 @@
+document.addEventListener('keydown',(e)=>{
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+    console.log(key);
+    
+   console.log(audio);
+   if(!audio) return ;
+   audio.currentTime = 0;
+   audio.play()
+   key.classList.add('playing')
+   
+
+   const keys = document.querySelectorAll('.key')
+   keys.forEach(key => key.addEventListener('transitionend',(e)=>{
+         if(e.propertyName !== 'transform') return;
+         key.classList.remove('playing')
+   }))
+    
+})
